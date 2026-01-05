eb467a3f2b6de60e2b9a01fdb84631079f9d6a54
404Setup <153366651+404Setup@users.noreply.github.com>
2026-01-05 18:01:49 +0800

    test update

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
 use jni::JNIEnv;
-use jni::objects::{JClass, JByteArray};
-use jni::sys::{jlong, jboolean, jint};
+use jni::objects::{JByteArray, JClass};
+use jni::sys::{jboolean, jint, jlong};
 use openssl::symm::{Cipher, Crypter, Mode};
 
-// Utility function to throw a Java exception
 fn throw_exception(env: &mut JNIEnv, exception_type: &str, message: &str) {
     let exception_class = env.find_class(exception_type).unwrap();
     env.throw_new(exception_class, message).unwrap();
@@ -16,41 +15,56 @@ pub extern "system" fn Java_com_velocitypowered_natives_encryption_OpenSslCipher
     key: JByteArray,
     encrypt: jboolean,
 ) -> jlong {
-    // Get the key bytes
     let key_len = match env.get_array_length(&key) {
         Ok(len) => len,
         Err(_) => {
-            throw_exception(&mut env, "java/lang/RuntimeException", "Failed to get array length");
+            throw_exception(
+                &mut env,
+                "java/lang/RuntimeException",
+                "Failed to get array length",
+            );
             return 0;
         }
     };
     if key_len != 16 {
-        throw_exception(&mut env, "java/lang/IllegalArgumentException", "cipher key not 16 bytes");
+        throw_exception(
+            &mut env,
+            "java/lang/IllegalArgumentException",
+            "cipher key not 16 bytes",
+        );
         return 0;
     }
 
-    // Convert the Java byte array to a Rust Vec<u8>
     let key_bytes = match env.convert_byte_array(&key) {
         Ok(bytes) => bytes,
         Err(_) => {
-            throw_exception(&mut env, "java/lang/RuntimeException", "Failed to convert byte array");
+            throw_exception(
+                &mut env,
+                "java/lang/RuntimeException",
+                "Failed to convert byte array",
+            );
             return 0;
         }
     };
 
-    // Create the crypter
-    let mode = if encrypt != 0 { Mode::Encrypt } else { Mode::Decrypt };
+    let mode = if encrypt != 0 {
+        Mode::Encrypt
+    } else {
+        Mode::Decrypt
+    };
     let cipher = Cipher::aes_128_cfb8();
 
-    // In Minecraft, the key is also used as the IV (as noted in the C implementation)
     match Crypter::new(cipher, mode, &key_bytes, Some(&key_bytes)) {
         Ok(crypter) => {
-            // Box the crypter and return a pointer to it
             let boxed = Box::new(crypter);
             Box::into_raw(boxed) as jlong
         }
         Err(_) => {
-            throw_exception(&mut env, "java/security/GeneralSecurityException", "openssl initialize cipher");
+            throw_exception(
+                &mut env,
+                "java/security/GeneralSecurityException",
+                "openssl initialize cipher",
+            );
             0
         }
     }
@@ -73,17 +87,14 @@ pub extern "system" fn Java_com_velocitypowered_natives_encryption_OpenSslCipher
     let src_buf = unsafe { std::slice::from_raw_parts(source as *const u8, len as usize) };
     let dst_buf = unsafe { std::slice::from_raw_parts_mut(dest as *mut u8, len as usize) };
 
-    // Process the data
-    let mut count = 0;
-    if let Ok(update_len) = crypter.update(src_buf, &mut dst_buf[count..]) {
-        count += update_len;
-    }
+    let update_len = if let Ok(len) = crypter.update(src_buf, dst_buf) {
+        len
+    } else {
+        0
+    };
 
-    // If there's any remaining data, finalize it
-    if count < len as usize {
-        if let Ok(final_len) = crypter.finalize(&mut dst_buf[count..]) {
-            count += final_len;
-        }
+    if update_len < len as usize {
+        let _ = crypter.finalize(&mut dst_buf[update_len..]);
     }
 }
 
@@ -93,8 +104,8 @@ pub extern "system" fn Java_com_velocitypowered_natives_encryption_OpenSslCipher
     _class: JClass,
     ctx: jlong,
 ) {
-    if ctx != 0 {
-        unsafe {
+    unsafe {
+        if ctx != 0 {
             let _ = Box::from_raw(ctx as *mut Crypter);
         }
     }
